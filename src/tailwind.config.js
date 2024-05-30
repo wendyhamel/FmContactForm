b8b6643b6359b786c5709d93b4316292a677fa6e
wendyhamel <whamel@it4web.nl>
2024-05-30 11:30:04 +0200

    Styling hover, active and focus states

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,6 +7,7 @@ tailwind.config = {
 			colors: {
 				'green-light': 'hsl(148, 38%, 91%)',
 				'green': 'hsl(169, 82%, 27%)',
+				'green-dark': 'hsl(172, 83%, 14%)',
 				'red': 'hsl(0, 66%, 54%)',
 				'gray': 'hsl(186, 15%, 59%)',
 				'gray-dark': 'hsl(187, 24%, 22%)',
